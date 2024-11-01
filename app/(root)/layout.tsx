71218bd9e083cfc756312bbb9a2f47802b5dbd72
sameerkindia <sameerkhanaa5@gmail.com>
2024-11-01 10:24:17 +0530

    Navbar

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,20 @@
+import Navar from "@/components/shared/navBar/Navar";
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="background-light850_dark100 relative">
+      <Navar />
+      <div className="flex">
+        left side bar
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+        right side bar
+      </div>
+      toster
+    </main>
+  );
+};
+
+export default Layout;
--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -10,21 +10,22 @@ interface ThemeContextType {
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  const [mode, setMode] = useState("");
+  const [mode, setMode] = useState("light");
 
   const handleThemeChange = () => {
     if (mode === "dark") {
       setMode("light");
       document.documentElement.classList.add("light");
-    } else {
+    } else if (mode === "light") {
       setMode("dark");
       document.documentElement.classList.add("dark");
     }
   };
 
   useEffect(() => {
-    handleThemeChange();
-  }, [mode]);
+    // handleThemeChange();
+    console.log("calling");
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ mode, setMode }}>
@@ -37,7 +38,7 @@ export function useTheme() {
   const context = useContext(ThemeContext);
 
   if (context === undefined) {
-    throw new Error("useTheme must be used within a themeProvider");
+    throw new Error("useTheme must be used within a ThemeProvider");
   }
 
   return context;
