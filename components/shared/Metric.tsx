a598535d38556234d456b1183e796236186abcd5
sameerkindia <sameerkhanaa5@gmail.com>
2024-11-06 10:30:14 +0530

    question card, no result and metric

---
+++ components/shared/Metric.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+interface MetricProps {
+  imgUrl: string;
+  alt: string;
+  value: number | string;
+  title: string;
+  href?: string;
+  textStyle?: string;
+  isAuther?: boolean;
+}
+
+const Metric = ({
+  imgUrl,
+  alt,
+  value,
+  title,
+  href,
+  textStyle,
+  isAuther,
+}: MetricProps) => {
+  const matricContent = (
+    <>
+      <Image
+        src={imgUrl}
+        alt={alt}
+        width={16}
+        height={16}
+        className={`object-contain ${href ? "rounded-full" : ""}`}
+      />
+
+      <p className={`${textStyle} flex items-center gap-1`}>
+        {value}
+        <span
+          className={`small-regular line-clamp-1 ${
+            isAuther ? "max-sm:hidden" : ""
+          }`}
+        >
+          {title}
+        </span>
+      </p>
+    </>
+  );
+
+  if (href) {
+    return (
+      <Link href={href} className="flex-center flex-wrap gap-1">
+        {matricContent}
+      </Link>
+    );
+  }
+
+  return <div className="flex-center flex-wrap gap-1">{matricContent}</div>;
+};
+
+export default Metric;
