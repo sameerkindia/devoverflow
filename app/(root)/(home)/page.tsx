5c4b3745f33929be8bf5bfe1759ad8835c25cd5e
sameerkindia <sameerkhanaa5@gmail.com>
2025-02-05 19:52:25 +0530

    local search

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -10,60 +10,14 @@ import QuestionCard from "@/components/cards/QuestionCard";
 import { getQuestion } from "@/lib/actions/question.action";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
+import { SearchParamsProps } from "@/types";
 
-// const questions = [
-//   {
-//     _id: "1",
-//     title:
-//       "The Lightning Component c:LWC_PizzaTracker generated invalid output for field status. Error How to solve this",
-//     tags: [
-//       { _id: "1", name: "javascript" },
-//       { _id: "2", name: "react.js" },
-//       { _id: "3", name: "invalid fields" },
-//       { _id: "4", name: "salesforce" },
-//     ],
-//     author: { _id: "1", name: "Sameer" },
-//     upvotes: 10,
-//     views: 100,
-//     answers: [],
-//     createdAt: new Date("2024-03-01"),
-//   },
-//   {
-//     _id: "2",
-//     title:
-//       "An HTML table where specific cells come from values in a Google Sheet identified by their neighboring cell",
-//     tags: [
-//       { _id: "1", name: "javascript" },
-//       { _id: "2", name: "react.js" },
-//       { _id: "3", name: "invalid fields" },
-//       { _id: "4", name: "salesforce" },
-//     ],
-//     author: { _id: "2", name: "Sameer khan" },
-//     upvotes: 10,
-//     views: 100,
-//     answers: [],
-//     createdAt: new Date("2024-05-08"),
-//   },
-//   {
-//     _id: "3",
-//     title:
-//       "JavaScript validation for a form stops the form data from being submitted to mysql database",
-//     tags: [
-//       { _id: "1", name: "javascript" },
-//       { _id: "2", name: "react.js" },
-//       { _id: "3", name: "invalid fields" },
-//       { _id: "4", name: "salesforce" },
-//     ],
-//     author: { _id: "3", name: "Sameer khan pali" },
-//     upvotes: 10,
-//     views: 55,
-//     answers: [],
-//     createdAt: new Date("2024-11-05"),
-//   },
-// ];
 
-const Home = async () => {
-  const result = await getQuestion({});
+
+const Home = async ({searchParams}: SearchParamsProps) => {
+  const result = await getQuestion({
+    searchQuery: searchParams.q
+  });
 
   const session = await auth()
 
